b719b474fba887f04dbee51d4aeaf38d6d2bc25c
rajesh8707 <rajesh2000alld@gmail.com>
2024-10-15 00:47:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+
+import List from "./component/List/List";
 
 function App() {
+
+  const [text, setText] = useState("");
+  const [todolist, setTodolist] = useState([]);
+
+  const addItem = () => {
+    const newToDoItem = {
+      id: uuidv4(),
+      item: text,
+      done: false,
+    };
+    setTodolist([...todolist, newToDoItem])
+    setText('');
+  };
+
+  const handleToggle = (itemId) => {
+    const newTodolist= todolist.map((listItem) => {
+      if(listItem.id === itemId){
+        return {...listItem,done: !listItem.done}
+      }
+      return listItem;
+    });
+    setTodolist(newTodolist); 
+  };
+  const handleDelete = (itemId) => {
+    const newTodolist = todolist.filter((listItem) => listItem.id !==itemId);
+    setTodolist(newTodolist);
+  }
+  console.log(todolist);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="App">
+        <h1>To Do List</h1>
+        <div className="adder">
+          <input type="text" placeholder="Add Items to list" value={text} onChange={(e) => setText(e.target.value)} />
+          <span>
+            <button onClick={addItem}>save</button>
+          </span>
+        </div>
+          {todolist.length > 0 && (
+            <List
+            todolist={todolist}
+            handleToggle={handleToggle}
+            handleDelete={handleDelete}
+            />
+          )}
+          
+        
+      
+      
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,7 @@
+import "./List.css";
+
+const List = () => {
+  return <ul className="list"></ul>;
+};
+
+export default List;
